35d6fbb33d8418bc414f9067d3d7605c6471f174
jadu1597 <94268397+jadu1597@users.noreply.github.com>
2025-04-03 22:14:09 +0900

    Add BFS solution to DFS_Sugeun.py

--- codes/week3_DFS/DFS_Sugeun.py
+++ codes/week3_DFS/DFS_Sugeun.py
@@ -24,3 +24,37 @@ def DFS(n, computers, com, visited):
 n = 3
 computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
 print(solution(n, computers))
+
+
+
+# Solution using BFS 
+# n개의 컴퓨터에 대해 배열 computers 를 입력받고 연결되어있는 네트워크의 개수 출력
+# 큐가 빌때까지 탐색, 비었다면 네트워크가 분리 된것
+# 방문하지 않은 컴퓨터를 찾아 새 네트워크 탐색
+# False로 이루어진 배열을 방문하여 True 로 바꾸는 과정
+
+# from collections import deque
+# def solution(n, computers):
+#     answer = 0
+#     visited = [False for _ in range(n)]   
+    
+#     for com in range(n):
+#         if visited[com] == False:
+#             BFS(n, computers, com, visited)
+#             answer += 1
+#     return answer
+    
+# def BFS(n, computers, com, visited):
+#     visited[com] = True
+
+#     queue = deque([com])                                                    # 큐에 시작노드 [com] 추가
+#     while queue:                                                            # 큐가 빌때까지
+#         node = queue.popleft()                                              # 큐에서 노드 꺼내기
+#         for i in range(n):
+#             if visited[i] == False and computers[i][node] == 1:                  
+#                 visited[i] = True
+#                 queue.append(i)
+
+# n = 3
+# computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+# print(solution(n, computers))
